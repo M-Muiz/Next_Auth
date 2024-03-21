eb2fe9100d1a743bfd38c9e3102d940f1104d226
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-22 01:41:47 +0500

    intialize project

---
+++ src/models/user/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verificationToken: String,
+    verificationTokenExpires: Date,
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpires: Date
+});
